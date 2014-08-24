fd69b53a9ea4fc25ff9ceecc7d0e586c806e84df
sapier <Sapier at GMX dot net>
2014-08-24 20:54:17 +0200

    Fix some errors due to split

--- animal_resources/weapons.lua
+++ animal_resources/weapons.lua
@@ -12,7 +12,7 @@
 --
 -- Contact sapier a t gmx net
 -------------------------------------------------------------------------------
-mobf_assert_backtrace(weapons_spacer == nil)
+assert(weapons_spacer == nil)
 local weapons_spacer = {} --unused to fix lua doxygen bug only
 
 -------------------------------------------------------------------------------
@@ -27,7 +27,7 @@ local weapons_spacer = {} --unused to fix lua doxygen bug only
 -------------------------------------------------------------------------------
 local function do_area_damage(pos,immune,damage_groups,range)
 	--damage objects within inner blast radius
-	mobf_assert_backtrace(type(range) ~= "table")
+	assert(type(range) ~= "table")
 
 	local objs = minetest.get_objects_inside_radius(pos, range)
 	for k, obj in pairs(objs) do
@@ -47,7 +47,7 @@ local function do_area_damage(pos,immune,damage_groups,range)
 			elseif type(damage_groups) == "number" then
 				worst_damage =  damage_groups
 			else
-				mobf_assert_backtrace("invalid damage_groups" == "selected")
+				assert("invalid damage_groups" == "selected")
 			end
 
 
@@ -81,14 +81,14 @@ local function do_node_damage(pos,immune_list,range,chance)
 			for k=pos.z-range,pos.z+range,1 do
 				--TODO create a little bit more sophisticated blast resistance
 				if math.random() < chance then
-					local toremove = minetest.get_node({x=i,y=j,z=k})
+					local toremove = core.get_node({x=i,y=j,z=k})
 
 					if toremove ~= nil then
 						local immune = false
 
 						if immune_list ~= nil then
 							for i,v in ipairs(immune_list) do
-								if (torremove.name == v) then
+								if (toremove.name == v) then
 									immune = true
 								end
 							end
@@ -96,7 +96,7 @@ local function do_node_damage(pos,immune_list,range,chance)
 
 
 						if immune ~= true then
-							minetest.remove_node({x=i,y=j,z=k})
+							core.remove_node({x=i,y=j,z=k})
 						end
 					end
 				end
@@ -105,7 +105,6 @@ local function do_node_damage(pos,immune_list,range,chance)
 	end
 end
 
-mobf_assert_backtrace(AR_FIREBALL_ENTITY == nil)
 --! @class AR_FIREBALL_ENTITY
 --! @ingroup weapons
 --! @brief a fireball weapon entity
@@ -142,7 +141,7 @@ local AR_FIREBALL_ENTITY = {
 --! @param staticdata
 -------------------------------------------------------------------------------
 function AR_FIREBALL_ENTITY.on_activate(self,staticdata)
-	self.created = mobf_get_current_time()
+	self.created = os.clock()
 end
 
 -------------------------------------------------------------------------------
@@ -157,28 +156,28 @@ end
 -------------------------------------------------------------------------------
 function AR_FIREBALL_ENTITY.surfacefire(pos,range)
 
-	if mobf_rtd.fire_enabled then
+	if mobf_rtd ~= nil and mobf_rtd.fire_enabled then
 		--start fire on any surface within inner damage range
 		for i=pos.x-range/2, pos.x+range/2, 1 do
 		for j=pos.y-range/2, pos.y+range/2, 1 do
 		for k=pos.z-range/2, pos.z+range/2, 1 do
 
-			local current = minetest.get_node({x=i,y=j,z=k})
-			local ontop  = minetest.get_node({x=i,y=j+1,z=k})
+			local current = core.get_node({x=i,y=j,z=k})
+			local ontop  = core.get_node({x=i,y=j+1,z=k})
 
 			--print("put fire? " .. printpos({x=i,y=j,z=k}) .. " " .. current.name .. " " ..ontop.name)
 
 			if (current.name ~= "air") and
 				(current.name ~= "fire:basic_flame") and
 				(ontop.name == "air") then
-				minetest.set_node({x=i,y=j+1,z=k}, {name="fire:basic_flame"})
+				core.set_node({x=i,y=j+1,z=k}, {name="fire:basic_flame"})
 			end
 
 		end
 		end
 		end
 	else
-		minetest.log(LOGLEVEL_ERROR,"MOBF: A fireball without fire mod??!? You're kidding!!")
+		core.log("error","AMR: A fireball without fire mod??!? You're kidding!!")
 	end
 end
 
@@ -194,11 +193,11 @@ end
 -------------------------------------------------------------------------------
 function AR_FIREBALL_ENTITY.on_step(self, dtime)
 	local pos = self.object:getpos()
-	local node = minetest.get_node(pos)
+	local node = core.get_node(pos)
 
 
 	--detect hit
-	local objs=minetest.get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 1)
+	local objs=core.get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 1)
 
 	local hit = false
 
@@ -230,13 +229,11 @@ function AR_FIREBALL_ENTITY.on_step(self, dtime)
 
 	--remove after lifetime has passed
 	if self.created > 0 and
-		self.created + self.lifetime < mobf_get_current_time() then
+		self.created + self.lifetime < os.clock() then
 		self.object:remove()
 	end
 end
 
-
-mobf_assert_backtrace(AR_PLASMABALL_ENTITY == nil)
 --! @class AR_PLASMABALL_ENTITY
 --! @ingroup weapons
 --! @brief a plasmaball weapon entity
@@ -274,7 +271,7 @@ local AR_PLASMABALL_ENTITY = {
 --! @param staticdata
 -------------------------------------------------------------------------------
 function AR_PLASMABALL_ENTITY.on_activate(self,staticdata)
-	self.created = mobf_get_current_time()
+	self.created = os.clock()
 end
 
 
@@ -290,11 +287,11 @@ end
 -------------------------------------------------------------------------------
 function AR_PLASMABALL_ENTITY.on_step(self, dtime)
 	local pos = self.object:getpos()
-	local node = minetest.get_node(pos)
+	local node = core.get_node(pos)
 
 
 	--detect hit
-	local objs=minetest.get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 1)
+	local objs=core.get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 1)
 
 	local hit = false
 
@@ -333,7 +330,7 @@ function AR_PLASMABALL_ENTITY.on_step(self, dtime)
 							toremove.name ~= "default:stone" and
 							toremove.name ~= "default:cobble" then
 
-							minetest.remove_node({x=i,y=j,z=k})
+							core.remove_node({x=i,y=j,z=k})
 						end
 					end
 				end
@@ -353,8 +350,6 @@ end
 -- -----------------------------------------------------------------------------
 -- Code Below is by far extent taken from throwing mod by PilzAdam
 -- -----------------------------------------------------------------------------
-
-mobf_assert_backtrace(AR_ARROW_ENTITY == nil)
 --! @class AR_ARROW_ENTITY
 --! @ingroup weapons
 --! @brief a arrow entity
@@ -388,10 +383,10 @@ local AR_ARROW_ENTITY={
 AR_ARROW_ENTITY.on_step = function(self, dtime)
 	self.timer=self.timer+dtime
 	local pos = self.object:getpos()
-	local node = minetest.get_node(pos)
+	local node = core.get_node(pos)
 
 	if self.timer>0.2 then
-		local objs = minetest.get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 2)
+		local objs = core.get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 2)
 		for k, obj in pairs(objs) do
 			if obj:get_luaentity() ~= nil and
 				obj ~= self.owner then
@@ -416,7 +411,7 @@ AR_ARROW_ENTITY.on_step = function(self, dtime)
 
 	if self.lastpos.x~=nil then
 		if node.name ~= "air" then
-			minetest.add_item(self.lastpos, 'animal_resources:arrow')
+			core.add_item(self.lastpos, 'animal_resources:arrow')
 			self.object:remove()
 		end
 	end
@@ -430,14 +425,14 @@ end
 --
 -------------------------------------------------------------------------------
 function ar_init_weapons()
-	minetest.register_entity(":animal_resources:fireball_entity", AR_FIREBALL_ENTITY)
-	minetest.register_entity(":animal_resources:plasmaball_entity", AR_PLASMABALL_ENTITY)
-	minetest.register_entity(":animal_resources:arrow_entity", AR_ARROW_ENTITY)
+	core.register_entity(":animal_resources:fireball_entity", AR_FIREBALL_ENTITY)
+	core.register_entity(":animal_resources:plasmaball_entity", AR_PLASMABALL_ENTITY)
+	core.register_entity(":animal_resources:arrow_entity", AR_ARROW_ENTITY)
 end
 
 
 -- moved due to doxygen problems
-minetest.register_node("animal_resources:arrow_box", {
+core.register_node("animal_resources:arrow_box", {
 	drawtype = "nodebox",
 	node_box = {
 		type = "fixed",
@@ -465,8 +460,11 @@ minetest.register_node("animal_resources:arrow_box", {
 	groups = {not_in_creative_inventory=1},
 })
 
-local mods = minetest.get_modnames()
-if mobf_contains(mods,"throwing") then
-	print("throwing mod found2!")
-	minetest.register_alias("animal_resources:arrow", "throwing:arrow")
+local mods = core.get_modnames()
+
+for i,v in ipairs(mods) do
+	if v == element then
+		print("AR: throwing mod found!")
+		core.register_alias("animal_resources:arrow", "throwing:arrow")
+	end
 end
